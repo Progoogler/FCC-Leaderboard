b19201ef00be40dd9e4361748ddfe607171ef6f2
Andy <andyzanli@gmail.com>
2017-08-08 10:53:03 -0700

    Included icons & genuine FCC green color

--- src/components/titleBar/TitleBar.js
+++ src/components/titleBar/TitleBar.js
@@ -6,6 +6,7 @@ class TitleBar extends Component {
   render() {
     return (
       <div className="Title-container">
+        <img className="Title-icon" src="../images/fcc-icon.png"/>
         <h2 className="Title">FreeCodeCamp</h2>
       </div>
     );
